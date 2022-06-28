fa98dbc576ac95be43540553ea236fb81e3ca3a9
Kathiravan T <quic_kathirav@quicinc.com>
2022-06-28 15:39:12 +0530

    arm64: dts: qcom: devsoc: Add the DCC support
    Add a seperate compatible string for the devsoc platform since there
    are few differences in the address space layout and registers. For instance,
    HW_TRIGGER_EN bit position in LL_CONFIG register is 8th in devsoc,
    whereas it is 9th in previous chipsets.
    
    Change-Id: Iaa4d12ee12ae2751c8a0b435f1430c2531d24e23
    Signed-off-by: Kathiravan T <quic_kathirav@quicinc.com>

--- drivers/soc/qcom/dcc_v2.c
+++ drivers/soc/qcom/dcc_v2.c
@@ -1804,7 +1804,8 @@ static int dcc_probe(struct platform_device *pdev)
 	if (ret)
 		return -EINVAL;
 
-	if (BVAL(dcc_readl(drvdata, DCC_HW_INFO), 9)) {
+	if (BVAL(dcc_readl(drvdata, DCC_HW_INFO), 9) ||
+			of_device_is_compatible(pdev->dev.of_node, "qcom,dcc-v2-devsoc")) {
 		drvdata->mem_map_ver = DCC_MEM_MAP_VER3;
 		drvdata->nr_link_list = dcc_readl(drvdata, DCC_LL_NUM_INFO);
 		if (drvdata->nr_link_list == 0)
@@ -1895,6 +1896,7 @@ static int dcc_remove(struct platform_device *pdev)
 
 static const struct of_device_id msm_dcc_match[] = {
 	{ .compatible = "qcom,dcc-v2"},
+	{ .compatible = "qcom,dcc-v2-devsoc"},
 	{}
 };
 

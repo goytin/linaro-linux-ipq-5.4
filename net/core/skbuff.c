ff79888912f059f6a75260a2c6eeb301c1157e60
Gaurao Chaudhari <quic_gaurchau@quicinc.com>
2023-01-17 15:36:45 -0800

    net: core: Do not change the truesize after skb allocation.
    This change makes two fixes in different areas:
    1) An earlier change with id I131f60e0e74627c4db916faaff76dc4fc3232996
    for older paltform improved the performance at the receiving end
    of a speedtest. The new platform works properly without that change.
    
    2) Revert the truesize calculation in pskb_expand_head function to calculate
    from previous buffer's truesize.
    
    Change-Id: I15054c235283138f270a307b05aefe5efcd5f5fd
    Signed-off-by: Gaurao Chaudhari <quic_gaurchau@quicinc.com>

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -471,11 +471,6 @@ struct sk_buff *__netdev_alloc_skb(struct net_device *dev,
 	bool reset_skb = true;
 	skb = skb_recycler_alloc(dev, length, reset_skb);
 	if (likely(skb)) {
-		/* SKBs in the recycler are from various unknown sources.
-		* Their truesize is unknown. We should set truesize
-		* as the needed buffer size before using it.
-		*/
-		skb->truesize = SKB_TRUESIZE(SKB_DATA_ALIGN(len + NET_SKB_PAD));
 		skb->recycled_for_ds = 0;
 		return skb;
 	}
@@ -488,12 +483,6 @@ struct sk_buff *__netdev_alloc_skb(struct net_device *dev,
 			  SKB_ALLOC_RX, NUMA_NO_NODE);
 	if (!skb)
 		goto skb_fail;
-
-	/* Set truesize as the needed buffer size
-	 * rather than the allocated size by __alloc_skb().
-	 */
-	if (length + NET_SKB_PAD < SKB_WITH_OVERHEAD(PAGE_SIZE))
-		skb->truesize = SKB_TRUESIZE(SKB_DATA_ALIGN(length + NET_SKB_PAD));
 
 	goto skb_success;
 #else
@@ -1986,7 +1975,7 @@ int pskb_expand_head(struct sk_buff *skb, int nhead, int ntail,
 	 * when skb is orphaned (not attached to a socket).
 	 */
 	if (!skb->sk || skb->destructor == sock_edemux)
-		skb->truesize = SKB_TRUESIZE(size);
+		skb->truesize += size - osize;
 
 	return 0;
 

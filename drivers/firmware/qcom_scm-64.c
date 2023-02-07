b395ef0ecf507c451d6424e8d23edb4deaa1adbe
Md Sadre Alam <quic_mdalam@quicinc.com>
2023-02-07 15:11:40 +0530

    firmware: qcom_scm: Fix number of args for 64-bit
    This change will fix number of argument to 2 for secure
    crypt smc call for 64-bit.
    
    Change-Id: I4317fea019fb018342d3d1d3b1b2c4ab4b354e54
    Signed-off-by: Md Sadre Alam <quic_mdalam@quicinc.com>

--- drivers/firmware/qcom_scm-64.c
+++ drivers/firmware/qcom_scm-64.c
@@ -1394,7 +1394,7 @@ int __qti_sec_crypt(struct device *dev, void *confBuf, int size)
 		dev_err(dev, "Allocation fail for conf buffer\n");
 		return -ENOMEM;
 	}
-	desc.arginfo = SCM_ARGS(1, QCOM_SCM_RW);
+	desc.arginfo = SCM_ARGS(2, QCOM_SCM_RW, QCOM_SCM_VAL);
 	desc.args[0] = (u64)conf_phys;
 	desc.args[1] = size;
 

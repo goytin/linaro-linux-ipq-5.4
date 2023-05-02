222d913d7f7b4bc9c5d54660e08607a190ab1edd
Timple Raj M <quic_timple@quicinc.com>
2023-05-02 13:47:01 +0530

    usb: phy: ipq5332: Fix to increase the CDR bandwidth
    This patch increases the CDR bandwidth to pass the
    USB 3.0 Rx jitter tolerance test
    
    Change-Id: Id2861d038fc9f323439627ee71da94a0d346fb01
    Signed-off-by: Timple Raj M <quic_timple@quicinc.com>

--- drivers/phy/qualcomm/phy-qca-uniphy.c
+++ drivers/phy/qualcomm/phy-qca-uniphy.c
@@ -50,10 +50,21 @@
 
 #define PHY_AUTOLOAD_PERIOD	35
 
-#define PCIE_USB_COMBO_PHY_CFG_RX_AFE_2	0x7C4
+#define PCIE_USB_COMBO_PHY_CFG_RX_AFE_2		0x7C4
+#define PCIE_USB_COMBO_PHY_CFG_RX_DLF_DEMUX_2	0x7E8
+#define PCIE_USB_COMBO_PHY_CFG_MISC1		0x214
+
 #define APB_REG_UPHY_RX_RESCAL_CODE	(16 << 8)
 #define APB_REG_UPHY_RX_AFE_CAP1	(7 << 4)
 #define APB_REG_UPHY_RX_AFE_RES1	(6 << 0)
+
+#define APB_REG_UPHY_RXD_BIT_WIDTH	(2 << 0)
+#define APB_REG_UPHY_RX_PLOOP_GAIN	(4 << 4)
+#define APB_REG_UPHY_RX_DLF_RATE	(1 << 8)
+#define APB_UPHY_RX_PLOOP_EN		(1 << 12)
+#define APB_REG_UPHY_RX_CDR_EN		(1 << 13)
+
+#define APB_REG_FLOOP_GAIN		(3 << 0)
 
 struct qca_uni_ss_phy {
 	struct phy phy;
@@ -122,6 +133,16 @@ static int qca_uni_ss_phy_init(struct phy *x)
 		APB_REG_UPHY_RX_AFE_CAP1 |
 		APB_REG_UPHY_RX_AFE_RES1,
 		phy->base + PCIE_USB_COMBO_PHY_CFG_RX_AFE_2);
+
+		writel(APB_REG_UPHY_RXD_BIT_WIDTH |
+		APB_REG_UPHY_RX_PLOOP_GAIN |
+		APB_REG_UPHY_RX_DLF_RATE |
+		APB_UPHY_RX_PLOOP_EN |
+		APB_REG_UPHY_RX_CDR_EN,
+		phy->base + PCIE_USB_COMBO_PHY_CFG_RX_DLF_DEMUX_2);
+
+		writel(APB_REG_FLOOP_GAIN,
+			phy->base + PCIE_USB_COMBO_PHY_CFG_MISC1);
 		return 0;
 	}
 
